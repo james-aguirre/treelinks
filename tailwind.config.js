04267bf6e733972d07fc430fb1991faf5ba9442c
James Aguirre <jamesaguirre98@gmail.com>
2024-01-05 03:45:34 -0800

    themes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,30 @@ export default {
 	theme: {
 		extend: {}
 	},
+	daisyui: {
+		themes: [
+			'light',
+			'dark',
+			'cupcake',
+			'bumblebee',
+			'emerald',
+			'synthwave',
+			'retro',
+			'cyberpunk',
+			'valentine',
+			'halloween',
+			'garden',
+			'forest',
+			'aqua',
+			'lofi',
+			'pastel',
+			'fantasy',
+			'wireframe',
+			'black',
+			'luxury',
+			'dracula'
+		]
+	},
+
 	plugins: [require('daisyui')]
 };
