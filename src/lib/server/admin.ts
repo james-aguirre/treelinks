1326fd043d06c7b476b2681899e3b83d74614eb2
James Aguirre <jamesaguirre98@gmail.com>
2024-01-05 02:59:07 -0800

    tailwind config

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -1,7 +1,12 @@
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private';
 import pkg from 'firebase-admin';
+import {
+	FB_CLIENT_EMAIL,
+	FB_PRIVATE_KEY,
+	FB_PROJECT_ID,
+	FB_SERVICE_ACCOUNT
+} from '$env/static/private';
 
 try {
 	pkg.initializeApp({
