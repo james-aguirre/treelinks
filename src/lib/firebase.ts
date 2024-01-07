b8ee717e326042dd9743e3a955fc1f284bed571d
James Aguirre <jamesaguirre98@gmail.com>
2024-01-07 13:25:06 -0800

    removed firebase analytics

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
@@ -17,7 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+    import { auth } from '$lib/firebase';
+    import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+
+    async function logInWithGoogle() {
+        const provider = new GoogleAuthProvider();
+        const user = await signInWithPopup(auth, provider);
+        console.log(user);  
+    }
 </script>
 
 <h2>Login</h2>
+<button on:click={logInWithGoogle} class="btn btn-primary">Sign in with google</button>
